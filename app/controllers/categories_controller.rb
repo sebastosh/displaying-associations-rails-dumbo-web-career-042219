6a181f5b9d3d578ca725516eb5cc72713e125bc1
sebastosh <sebastosh@gmail.com>
2019-05-20 20:36:57 -0400

    Done.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,7 +12,7 @@ class CategoriesController < ApplicationController
   end
 
   def create
-    category = Category.create(params[:category])
+    category = Category.create(cat_params)
     redirect_to category_path(category)
   end
 
@@ -22,7 +22,13 @@ class CategoriesController < ApplicationController
 
   def update
     category = Category.find(params[:id])
-    category.update(params.require(:category))
+    category.update(cat_params)
     redirect_to category_path(category)
+  end
+
+  private
+
+  def cat_params
+    params.require(:category).permit(:name)
   end
 end
